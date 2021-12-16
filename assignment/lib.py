cf2ca7dd85a533eec307786285adb9923b40abaf
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-12-16 17:58:40 +0100

    `opt_temperature.py` is now free of runtime errors, but things are not okay
    Also, we have not checked pathological cases that might cause runtime errors.
    Also, there was a runtime warning where an invalid value was encountered in the
    static method. Most likely the argument of `np.exp` became large in magnitude

---
+++ assignment/lib.py
@@ -0,0 +1,10 @@
+from assignment.TSP import *
+from assignment.SAMinimizer import *
+from assignment.cooling import *
+from assignment.opt_temperature import *
+from assignment.util import *
+# from TSP import *
+# from SAMinimizer import *
+# from cooling import *
+# from opt_temperature import *
+# from util import *
--- assignment/opt_temperature.py
+++ assignment/opt_temperature.py
@@ -1,20 +1,21 @@
-from assignment.TSP import *
+from assignment.lib import *
 import numpy as np
 
-# TODO: Test this whole thing
+# FIXME
+# Doesn't appear to be working as intended. Debug later if life permits
 """
     
 """
 
 class TemperatureOptimizer:
-    def __init__(self, cost_function, delta_cost_function, state_constructor):
+    def __init__(self, cost_function, delta_cost_function):
         self.cost = cost_function
-        self.delta_cost delta_cost_function
+        self.delta_cost = delta_cost_function
         self.T_iter_values = []
 
     
     def generate_states(self, number_of_states):
-        n = self.cost.D[0]
+        n = self.cost.D.shape[0]
         states_list = []
         for _ in range(number_of_states):
             state = np.random.permutation(n)
@@ -31,21 +32,48 @@ class TemperatureOptimizer:
                 d_cost = self.delta_cost(state, i, k)
                 if d_cost > 0:
                     new_cost = costs[l] + d_cost
-                    Emaxs[l] = d_cost
+                    Emaxs[l] = new_cost
                     break
         return costs, Emaxs
 
-    def iterate_temperatures(self, T0, iter_count, Emins, Emaxs, target_acceptance_rate):
+    def iterate_temperatures(self, T0, Emins, Emaxs, target_acceptance_rate, eps = 0.1, max_iter = 10**5, p = 1):
+        # TODO: verify
+        chi0 = target_acceptance_rate
         logchi0 = np.log(target_acceptance_rate)
         T = T0
-        for _ in range(iter_count):
-            logchi = np.log(self.__class__.chi(T0, Emins, Emaxs))
-            T *= logchi/logchi0 
+        i = 0
+        chi_n = self.__class__.chi(T, Emins, Emaxs)
+        while (i < max_iter and np.abs(chi0 - chi_n) > eps):
+            logchi = np.log(chi_n)
+            T *= np.power(logchi/logchi0, p)
             self.T_iter_values.append(T) 
+            chi_n = self.__class__.chi(T, Emins, Emaxs)
+
+    def iterate_from_scratch(self, S, T0, target_acceptance_rate, eps = 0.1, max_iter = 10**5):
+        states_list, Emins = self.generate_states(S)
+        Emins, Emaxs = self.generate_positive_transitions(states_list, Emins)
+        self.iterate_temperatures(T0, Emins, Emaxs, target_acceptance_rate, eps = eps, max_iter = max_iter)
 
 
     @staticmethod
     def chi(T, Emins, Emaxs):
+        # TODO: Verify
         numerator = np.sum(np.exp(-Emaxs/T))
         denominator = np.sum(np.exp(-Emins/T))
         return numerator/denominator
+
+
+
+"""
+if __name__ == '__main__':
+    cost_func, dcost_func = COST_FUNCTION_PAIRS[0]
+    temp_opter = TemperatureOptimizer(cost_func, dcost_func)
+    S = 100
+    states_list, Emins = temp_opter.generate_states(100)
+    Emins, Emaxs = temp_opter.generate_positive_transitions(states_list, Emins)
+    T0 = 100.0
+    iter_count = 100 
+    temp_opter.iterate_temperatures(T0, iter_count, Emins, Emaxs, 0.8)
+    print(temp_opter.T_iter_values)
+
+"""
