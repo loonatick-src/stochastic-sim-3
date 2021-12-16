5c5c3b9f7942f338e85144ba19b06fad09002301
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-12-16 14:05:27 +0100

    Add other cooling schedules along with some tests

--- assignment/cooling.py
+++ assignment/cooling.py
@@ -1,5 +1,9 @@
 import unittest
+import numpy as np
 
+# No, I am not a Java programmer
+# I was not sure how complicated the cooling schedules could get
+# So, tried to make a generalized API. Varying degrees of success
 class CoolingSchedule:
     def __init__(self, func, *args):
         self.f = func
@@ -11,8 +15,30 @@ class CoolingSchedule:
 
 class GeometricCoolingSchedule(CoolingSchedule):
     def __init__(self, cr):
+        assert 0 < cr < 1
         self.args = (None,)
         self.f = lambda T, _, k: T * cr**k
+
+
+class LinearCoolingSchedule(CoolingSchedule):
+    def __init__(self, alpha):
+        self.args = (None,)
+        self.f = lambda T, _, k: T - k*alpha
+
+
+class LogarithmicCoolingSchedule(CoolingSchedule):
+    def __init__(self, alpha):
+        assert 0 < alpha < 1
+        self.args = (None,)
+        self.f = lambda T, _, k: T/(1 + alpha * np.log(1+k))
+
+
+class ExponentialCoolingSchedule(CoolingSchedule):
+    def __init__(self, beta):
+        assert 0 < beta < 1
+        self.args = (None,)
+        self.f = lambda T, _, k: T * np.exp(-beta * k)
+
 
 class CoolingTestMethods(unittest.TestCase):
     def test_geometric(self):
@@ -24,6 +50,33 @@ class CoolingTestMethods(unittest.TestCase):
             T = cooling_schedule(T0, i)
             self.assertTrue(abs(T - c * T_lag) <= 1.0e-6)
             T_lag = T
+
+
+    def test_geometric_fail(self):
+        c = 1.1
+        with self.assertRaises(AssertionError):
+            cooling_schedule = GeometricCoolingSchedule(c)
+
+
+
+    def test_linear(self):
+        T0 = 100.0
+        alpha = 1.5
+        cooling_schedule = LinearCoolingSchedule(alpha)
+        T_lag = T0
+        for i in range(1,10):
+            T = cooling_schedule(T0, i)
+            self.assertTrue(abs(T - T_lag + alpha) < 1.0e-6)
+            T_lag = T
+
+    def test_exponential(self):
+        T0 = 100.0
+        beta = 0.5
+        cooling_schedule = ExponentialCoolingSchedule(beta)
+        for i in range(1,10):
+            T = cooling_schedule(T0, i)
+            self.assertEqual(T0 * np.exp(-beta * i), T)
+
 
 
 if __name__ == '__main__':
